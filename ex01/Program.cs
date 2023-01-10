218fac1d2e4378e79532122bdff22e3aeee0d4bc
Doug7bw <110914284+Doug7bw@users.noreply.github.com>
2023-01-10 16:25:17 -0300

    Add files via upload

---
+++ ex01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex01;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count;
+
+        Console.WriteLine("Digite um número inteiro: ");
+        count = int.Parse(Console.ReadLine());
+
+        for(int n = 1; n <= count; n++)
+        {
+            if((n % 2) != 0)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ex02;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int valor, numero, intervaloIn = 0, intervaloOut = 0;
+
+        Console.WriteLine("Digite um número inteiro: ");
+        valor = int.Parse(Console.ReadLine());
+
+        for(int n = 1; n <= valor; n++)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero >= 10 && numero <= 20)
+            {
+                intervaloIn++;
+            }
+            else
+            {
+                intervaloOut++;
+            };
+        };
+
+        Console.WriteLine($"{intervaloIn} in");
+        Console.WriteLine($"{intervaloOut} out");
+    }
+}
