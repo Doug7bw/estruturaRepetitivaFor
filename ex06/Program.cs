655379da5461a39505878baa754478651f26e03c
Doug7bw <110914284+Doug7bw@users.noreply.github.com>
2023-01-12 11:15:26 -0300

    Add files via upload

---
+++ ex06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex06;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero, divisor, n;
+
+        Console.WriteLine("Digite um número inteiro: ");
+        numero = int.Parse(Console.ReadLine());
+
+        for(n = 1; n <= numero ; n++)
+        {
+            if(numero % n == 0)
+            {
+                Console.WriteLine(n);
+            };
+        };
+    }
+}
---
+++ ex07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex07;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero, n;
+
+        Console.WriteLine("Digite um número inteiro: ");
+        numero = int.Parse(Console.ReadLine());
+        
+        for(n = 1; n <= numero; n++)
+        {
+            Console.WriteLine($"{n} {Math.Pow(n, 2)} {Math.Pow(n, 3)}");
+        };
+    }
+}
